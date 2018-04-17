66476b16813e094eff6731d79576dc69cbdcc4ca
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-04-17 18:08:06 -0400

    initial commit. library and book undefined

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-// write your code here to make the tests pass
+var Library = function () {
+  var books = [];
+
+  var addBook = function (book) {
+    books.push(book);
+  }
+  var checkOutBook = function () {
+    if(attributes[checkedOut] == false) {
+      attributes[checkedOut] = true);
+    }
+  }
+  return {
+    addBook: addBook,
+  }
+};
+var Book = function (name, author) {
+  var attributes = {
+    name: name,
+    author: author,
+   checkedOut: false
+  };
+
+  var getAttribute = function (attribute) {
+    if (attributes[attribute]) {
+      return attributes[attribute];
+    }
+  };
+  var setAttribute = function (attribute, value) {
+    if (attributes[attribute]) {
+     attributes[attribute] = value;
+   }
+};
+  return {
+    get: getAttribute,
+    set: setAttribute,
+    checkedOut: checkedOut
+  };
+};
+}
