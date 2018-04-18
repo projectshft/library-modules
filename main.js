664e11cea9cbbf9d186e94ec4f0990ee3efce4ec
Altneuj <34726282+Altneuj@users.noreply.github.com>
2018-04-18 18:37:10 -0400

    first commit

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
-var Library = function () {
+var Library = function() {
   var books = [];
 
-  var addBook = function (book) {
+  var addBook = function(book) {
     books.push(book);
   }
-  var checkOutBook = function () {
-    if(attributes[checkedOut] == false) {
-      attributes[checkedOut] = true);
+  var returnBook = function(book) {
+    if (attributes[checkedOut] == true) {
+      attributes[checkedOut] = false
+    }
+  };
+  var checkOutBook = function() {
+    if (attributes[checkedOut] == false) {
+      attributes[checkedOut] = true;
     }
   }
+
   return {
     addBook: addBook,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
   }
 };
-var Book = function (name, author) {
+
+var Book = function(name, author) {
   var attributes = {
     name: name,
     author: author,
-   checkedOut: false
+    checkedOut: false
   };
 
-  var getAttribute = function (attribute) {
+  var getAttribute = function(attribute) {
     if (attributes[attribute]) {
       return attributes[attribute];
     }
   };
-  var setAttribute = function (attribute, value) {
+  var setAttribute = function(attribute, value) {
     if (attributes[attribute]) {
-     attributes[attribute] = value;
-   }
-};
+      attributes[attribute] = value;
+    }
+  };
   return {
     get: getAttribute,
     set: setAttribute,
     checkedOut: checkedOut
   };
 };
-}
