4fab63f04051ba5bc76442bc095a5667db0030a3
davieu <davisumana@gmail.com>
2018-04-23 07:34:44 -0400

    finished commit

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
-// write your code here to make the tests pass
+var Library = function () {
+    var books = [];
+
+     var addBook = function (book) {
+        books.push(book);
+    };
+
+    var checkOutBook = function (book) {
+      if(books.indexOf(book)!= -1){
+          book.set("checkedOut", true);
+          }
+
+    };
+
+    var returnBook = function(book) {
+      if(books.indexOf(book)!= -1) {
+        book.set("checkedOut", false);
+        }
+      };
+
+    return {
+      addBook: addBook,
+      checkOutBook : checkOutBook,
+      returnBook: returnBook
+    };
+};
+
+var Book = function (theTitle, theAuth) {
+  var detail = {
+  title: theTitle,
+  author: theAuth,
+  checkedOut: false
+  };
+
+    var set = function(key, value){
+      if (detail.hasOwnProperty(key)) {
+      detail[key] = value;
+    }};
+
+    var get = function(item){
+      if (detail.hasOwnProperty(key)) {
+      return detail[key];
+    }};
+
+    return {
+      set: set,
+      get: get
+    };
+
+};
