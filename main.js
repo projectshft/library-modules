b9b8699cbcbb4311cc2d419b0a6847c8b4b0f945
Edward Barton <edward.barton@icloud.com>
2018-10-30 18:14:29 -0400

    completed exercises to pass tests

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
-// write your code here to make the tests pass
+var Library = function() {
+  var books = [];
+
+  var addBook = function(book) {
+    books.push(book);
+  };
+
+  var listBooks = function() {
+    for(var i = 0; i < books.length; i++) {
+      console.log(books[i].getAttribute('name'));
+    }
+  };
+
+  var checkOutBook = function(book) {
+    for(var i = 0; i < books.length; i++) {
+      if(books[i] === book) {
+        books[i].setAttribute('checkedOut', true);
+      }
+    }
+  };
+
+  var returnBook = function(book) {
+    for(var i = 0; i < books.length; i++) {
+      if(books[i] === book) {
+        books[i].setAttribute('checkedOut', false);
+      }
+    }
+  }
+
+  return {
+    addBook: addBook,
+    listBooks: listBooks,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
+  };
+};
+
+var Book = function(title, author) {
+  var attributes = {
+    title: title,
+    author: author,
+    checkedOut: false
+  };
+
+  var getAttribute = function(attribute) {
+    if(Object.keys(attributes).includes(attribute)) {
+      return attributes[attribute];
+    }
+  };
+
+  var setAttribute = function(attribute, value) {
+    if(Object.keys(attributes).includes(attribute)) {
+      attributes[attribute] = value;
+    }
+  };
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  };
+};
