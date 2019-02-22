0c957ebe2aaf1d38c05343d6dc14e818add13750
marielklem <mariel.steinbeiser@gmail.com>
2019-02-22 08:55:10 -0500

    fix all errors so all tests would pass

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-// write your code here to make the tests pass
+var Library = function() {
+  var books = [];
+
+  var addBook = function(book) {
+    books.push(book);
+  };
+
+  var checkOutBook = function(book) {
+    if (books.includes(book)) {
+      book.setAttribute('checkedOut', true);
+  };
+  };
+  var returnBook = function(book) {
+    if (books.includes(book)) {
+    book.setAttribute('checkedOut', false);
+  };
+  };
+
+  return {
+    addBook : addBook,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
+  };
+};
+
+var Book = function(title, author) {
+  var attributes = {
+    title: title,
+    author: author,
+    checkedOut: false
+  };
+  var getAttribute = function(attribute) {
+     if (attributes[attribute] != undefined) {
+       return attributes[attribute];
+     }
+   };
+   var setAttribute = function(attribute, value) {
+     if (attributes[attribute] != undefined) {
+       attributes[attribute] = value;
+     }
+  };
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  };
+};
+books.includes
