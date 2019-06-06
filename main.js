3817d1ec051c3a6a5a768f51edcf0f70b13b4f68
unknown <nathaniel.poli@gmail.com>
2019-06-06 08:58:35 -0400

    All jasmine tests passing

--- main.js
+++ main.js
@@ -1,42 +1,69 @@
-// write your code here to make the tests pass
-var Library = function() {
-  var books = [];
+  // write your code here to make the tests pass
+  debugger;
 
-  var addBook = function(book) {
-    books.push(book);
-  };
+  var Library = function() {
+    var books = [];
 
-  var checkOutBook = function(book) {
-    book.setAttribute('checkedOut', true);
-  }
+    var addBook = function(book) {
+      books.push(book);
+    };
 
-  return {
-    addBook: addBook,
-    checkOutBook : checkOutBook
-  }
-};
+    var checkOutBook = function(book) {
+      if (books.includes(book)) {
+      book.setAttribute('checkedOut', true);
+      }
+    }
 
-var Book = function(title, author) {
-  var attributes = {
-    title : title,
-    author : author,
-    checkedOut : false
-  };
+    var listBooks = function() {
+      for (var i = 0; i < books.length; i++) {
+      console.log(books[i]);
+      }
+    }
 
-  var getAttribute = function(attribute) {
-    if (attributes[attribute]) {
-      return attributes[attribute];
+    var returnBook = function(book) {
+      if (books.includes(book)) {
+        book.setAttribute('checkedOut', false);
+      }
+    }
+
+    return {
+      addBook: addBook,
+      checkOutBook : checkOutBook,
+      listBooks : listBooks,
+      returnBook : returnBook
     }
   };
 
-  var setAttribute = function(attribute, value) {
-    if (attributes[attribute]) {
-      attributes[attribute] = value;
-    }
+  var Book = function(title, author) {
+    var attributes = {
+      title : title,
+      author : author,
+      checkedOut : false
+    };
+
+    var getAttribute = function(attribute) {
+      if (attributes[attribute]) {
+        return attributes[attribute];
+      }
+      else if (attributes["checkedOut"] == false  && attribute == 'checkedOut') {
+        return attributes[attribute];
+      }
+    };
+
+    var setAttribute = function(attribute, value) {
+      if (attributes[attribute]) {
+        attributes[attribute] = value;
+      }
+      else if (attributes["checkedOut"] == false  && attribute == 'checkedOut') {
+        attributes[attribute] = value;
+      }
+    };
+
+    return {
+      getAttribute : getAttribute,
+      setAttribute : setAttribute
+    };
   };
 
-  return {
-    getAttribute : getAttribute,
-    setAttribute : setAttribute
-  };
-};
+  var superBook = Book('big book', 'mr twister');
+  superBook.getAttribute('checkedOut');
