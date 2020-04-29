297e036e880c2fda5917d2f27afae80530261d98
Spalding Vance <spaldingvance@Spaldings-MBP.nc.rr.com>
2020-04-29 15:16:42 -0400

    finished exercise

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-// write your code here to make the tests pass
+var Library = function() {
+  var books = [];
+
+  var addBook = function(book) {
+    books.push(book);
+  };
+
+  var checkOutBook = function(book) {
+    if(books.includes(book)) {
+      book.setAttribute("checkedOut", true);
+    } else {
+      console.log("Book cannot be checked out as it is already checked out");
+    }
+  };
+
+  var returnBook = function(book) {
+    if(books.includes(book)) {
+      book.setAttribute("checkedOut", false);
+    }
+  };
+
+  return {
+    addBook: addBook,
+    checkOutBook: checkOutBook,
+    returnBook: returnBook
+  };
+};
+
+var Book = function(title, author) {
+
+  var attributes = {
+    title: title,
+    author: author,
+    checkedOut: false
+  };
+
+  var getAttribute = function(attribute) {
+    return attributes[attribute];
+  };
+
+  var setAttribute = function(attribute, value) {
+    if(Object.keys(attributes).includes(attribute)) {
+      attributes[attribute] = value;
+    }
+  };
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  };
+};
