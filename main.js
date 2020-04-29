5fdd0260d02220b9197111d64b62bdf0967fdd08
Pierre Lourens <plourens3@gmail.com>
2020-04-28 22:21:03 -0400

    Initial attempt

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-// write your code here to make the tests pass
+var Library = function() {
+  var books = [];
+
+  var addBook = function(book) {
+    books.push(book);
+  };
+
+  var checkOutBook = function(book) {
+    if (books.includes(book)) {
+      book.setAttribute("checkedOut", true);
+    }
+  };
+
+  var returnBook = function(book) {
+    if (books.includes(book)) {
+      book.setAttribute("checkedOut", false);
+    }
+  };
+
+  return {
+    addBook,
+    checkOutBook,
+    returnBook,
+  };
+};
+
+var Book = function(title, author) {
+  var attributes = {
+    title,
+    author,
+    checkedOut: false,
+  };
+
+  var getAttribute = function(attribute) {
+    return attributes[attribute];
+  };
+
+  var setAttribute = function(attribute, status) {
+    if (attribute in attributes) {
+      attributes[attribute] = status;
+    }
+  };
+
+  return {
+    getAttribute,
+    setAttribute,
+  };
+};
